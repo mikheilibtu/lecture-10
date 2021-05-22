9a2b788c1eed30efa89e66ff51db25dbdffe913b
Mikheili <lomidzemikheili@gmail.com>
2021-05-22 12:10:19 +0400

    Add group 14 practical work.

--- group_14/main.py
+++ group_14/main.py
@@ -9,13 +9,14 @@
 2. ინფორმაციის შეგროვების მერე დავადგინოთ სტატისტიკური მახასიათებლები ავტორების შესახებ:
 - სულ რამდენი ავტორია ვებსაიტზე
 - რომელ ავტორის რამდენი გამონათქვამი ეკუთვნის
-	დამუშავებული ინფორმაცია ჩავწეროთ ფაილურ სისტემაში JSON ფორმატში.
+    დამუშავებული ინფორმაცია ჩავწეროთ ფაილურ სისტემაში JSON ფორმატში.
 
-3. დავადგინოთ სტატისტიკური მახასიათებლები ტააგების შესახებ:
+3. დავადგინოთ სტატისტიკური მახასიათებლები ტაგების შესახებ:
 - სულ რამდენი ტაგი შეგვხვდა ვებსაიტზე
 - თითოეული ტაგი რამდენჯერ გვხვდება
 - 5 ყველაზე პოპულარული ტაგი
-	დამუშავებული ინფორმაცია ჩავწეროთ ფაილურ სისტემაში JSON ფორმატში.
+    დამუშავებული ინფორმაცია ჩავწეროთ ფაილურ სისტემაში JSON ფორმატში.
+
 """
 import json
 from pprint import pprint
@@ -71,6 +72,59 @@ class DataScraper:
         pprint(results)
 
 
+def calculate_author_stats(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    authors_dict = {}
+    for document in data:
+        author = document.get("author")
+        if author not in authors_dict:
+            authors_dict[author] = 1
+        else:
+            authors_dict[author] = authors_dict[author] + 1
+    authors_count = len(authors_dict.keys())
+    result = {
+        "count": authors_count,
+        "stats": authors_dict
+    }
+    save_as_json("author_stats.json", result)
+
+
+def save_as_json(fname, result):
+    with open(fname, "w") as file:
+        json.dump(result, file, indent=4)
+
+
+def calculate_tags_stats(filename):
+    with open(filename, "r") as file:
+        data = json.load(file)
+    tags_dict = {}
+    for document in data:
+        tags = document.get("tags", [])
+        for tag in tags:
+            if tag in tags_dict:
+                tags_dict[tag] = tags_dict[tag] + 1
+            else:
+                tags_dict[tag] = 1
+    tags_count = len(tags_dict.keys())
+    pprint(tags_dict)
+    _sorted = sorted([(k, v) for k, v in tags_dict.items()], key=lambda pair: pair[1], reverse=True)
+    _sorted = _sorted[:5]
+    top_5 = [k for k, v in _sorted]  # 1M operation 6 GB
+
+    result = {
+        "tags_count": tags_count,
+        "stats": tags_dict,
+        "top_5": top_5
+    }
+
+    save_as_json("tags_stats.json", result)
+
+
 if __name__ == '__main__':
     data_scraper = DataScraper()
+
     data_scraper.load_data_to_json()
+
+    calculate_author_stats("result.json")
+    calculate_tags_stats("result.json")
